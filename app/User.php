1a2997d67128c5a782562880065ce8fc33708b1a
abdalgaderserag <abdalgadersir@gmail.com>
2019-07-11 03:21:56 -0700

    add avatar for the user and adding some avatar images

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'slug', 'email', 'password', 'avatar', 'api_token'
+        'name', 'email', 'password', 'avatar',
     ];
 
     /**
