4bf7b5bd39cc5261bfdfe0482bc9e3676996960a
abdalgaderserag <abdalgadersir@gmail.com>
2019-07-14 08:16:16 -0700

    added factory to the models

--- database/factories/TransferFactory.php
+++ database/factories/TransferFactory.php
@@ -4,6 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Transfer::class, function (Faker $faker) {
     return [
-        //
+        'amount' => random_int(1,23) * 100,
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 5)->create()->each(function ($u) {
+        $avatars = [
+            'a902d1d3-9692-412a-a2fc-a168a716f8be.png',
+            'ariya.jpg',
+            'astr.png',
+            'c674bdef-feab-44a7-a40a-71cc66b4af8f.png',
+            'defaul.jpeg',
+            'default.jpeg',
+            'freakboy3742.jpg',
+            'photo-1527980965255-d3b416303d12.jpeg',
+            'yyx990803.jpg'
+        ];
+        for ($j = 0; $j < 5; $j++) {
+            $u = factory(App\User::class)->make();
+            $u->save();
+            $u->avatar = 'images/profile/' . $avatars[$u->id - 1];
+            $u->save();
             $money = factory(App\Money::class)->make();
             $money->user_id = $u->id;
             $money->save();
-        });
+            for ($i = 0; $i < random_int(12, 20); $i++) {
+                $transfer = factory(\App\Transfer::class)->make();
+                $transfer->sender_id = $u->id;
+                $transfer->recipient_id = random_int(1, 5);
+                if ($transfer->sender_id !== $transfer->recipient_id)
+                    $transfer->save();
+            }
+        };
     }
 }
