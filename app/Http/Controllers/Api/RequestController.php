11adc73164289784fca07405de7c269ea97c7907
abdalgaderserag <abdalgadersir@gmail.com>
2019-07-17 12:01:14 -0700

    add basic stuff for the request money model

---
+++ app/Http/Controllers/Api/RequestController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\RequestMoney;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\RequestMoney  $requestMoney
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RequestMoney $requestMoney)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\RequestMoney  $requestMoney
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RequestMoney $requestMoney)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\RequestMoney  $requestMoney
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RequestMoney $requestMoney)
+    {
+        //
+    }
+}
