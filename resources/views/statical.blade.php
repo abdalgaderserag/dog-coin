e4070e6d8d106e49a9398063031e9e489b7d73a1
abdalgaderserag <abdalgadersir@gmail.com>
2019-07-17 02:14:22 -0700

    start working at statical view

--- resources/views/statical.blade.php
+++ resources/views/statical.blade.php
@@ -3,4 +3,12 @@
 @section('title','Dashboard')
 
 @section('content')
+    <div class="container flex-box">
+
+        @include('layouts.left-side')
+
+        <div class="main" style="width: 60%;height: 260px;padding-top: 42px">
+            <statical-view :data="{{ \App\Transfer::all() }}" :max="1800"></statical-view>
+        </div>
+    </div>
 @endsection
