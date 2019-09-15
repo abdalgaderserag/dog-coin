c819c65358e425142075d314085b1e376c37741b
abdalgaderserag <abdalgadersir@gmail.com>
2019-09-15 07:45:06 -0700

    cors

--- app/Http/Middleware/ShopCors.php
+++ app/Http/Middleware/ShopCors.php
@@ -16,11 +16,8 @@ class ShopCors
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', [
-                'http://127.0.0.1:8000',
-                'http://127.0.0.1:8000/profile/*'
-            ])
+            ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+            ->header('Access-Control-Allow-Headers', '*');
     }
 }
